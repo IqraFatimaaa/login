075d896f7a80914492ae061d3d53735c4db83765
Iqra Nadeem <98485928+IqraFatimaaa@users.noreply.github.com>
2024-01-01 02:08:35 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Table from './Table'
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      
+         <h1> Table</h1> 
+       
+        <Table/>
+    </div>
+  );
+}
+
+export default App;
